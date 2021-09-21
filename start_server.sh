25c639b15e579cc621ac1fb8e21587045164216d
NateDukatz <natedukatz@gmail.com>
2021-09-21 15:54:43 -0400

    add get dependencies and compiling to script

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,5 @@
+mix deps.get
+elixirc test/test_helper.ex
+elixirc test/tools/util.ex 
 docker pull selenium/standalone-chrome
 docker run -d -p  4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome
